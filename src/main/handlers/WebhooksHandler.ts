f9451424fe8ae6111c2869d3954a911bd6144011
Zia Ur Rehman Khan <zmajrohi323@gmail.com>
2023-03-17 05:16:07 +0500

    WIP: send payload to meveto.

--- src/main/handlers/WebhooksHandler.ts
+++ src/main/handlers/WebhooksHandler.ts
@@ -1,3 +1,7 @@
+import fs from 'fs';
+import path from 'path';
+import { createHash, sign } from 'crypto';
+import nconf from 'nconf';
 import { SyncAction } from '../../renderer/pages/Home';
 
 export const WEBHOOK = {
@@ -13,29 +17,33 @@ type SendPayloadResponse = {
 type DispatchActions = { [K in SyncAction]: () => void };
 
 interface DispatchActionProps extends DispatchActions {
-  getCsvFile: () => void;
+  preparePayload: () => {
+    id: string;
+    type: SyncAction | 'deleteUsers' | 'deleteGroups';
+    checksum: string;
+    fileName: string;
+    signature: string;
+  };
 }
 
-const dispatchActions: DispatchActionProps = {
-  getCsvFile: function () {
-    //
-  },
-
-  partialGroups: function () {
-    //
-  },
-
-  fullGroups: function () {
-    //
-  },
-
-  partialUsers: function () {
-    //
-  },
-
-  fullUsers: function () {
-    //
-  },
+const dispatchActions = {
+  // preparePayload: function () {
+  //   return {
+  //     id: nconf.get('appID'),
+  //   };
+  // },
+  // partialGroups: function () {
+  //   //
+  // },
+  // fullGroups: function () {
+  //   //
+  // },
+  // partialUsers: function () {
+  //   //
+  // },
+  // fullUsers: function () {
+  //   //
+  // },
 };
 
 /**
@@ -44,6 +52,54 @@ const dispatchActions: DispatchActionProps = {
  * @param payloadType Type of the payload is the same as the action being performed.
  * @param fileName Name of the exported file by PowerShell that should be sent to Meveto.
  */
-export const sendPayload = (payloadType: SyncAction, fileName: string): SendPayloadResponse => {
+export const sendPayload = async (
+  payloadType: SyncAction | 'deleteUsers' | 'deleteGroups',
+  fileName: string
+): Promise<SendPayloadResponse> => {
+  // Check if the given file exists.
+  const programFilesPath = process.env.ProgramFiles || 'C:\\Program Files';
+  const mevetoExportsPath = path.join(programFilesPath, 'Meveto', 'Exports');
+  const filePath = path.join(mevetoExportsPath, fileName);
+
+  try {
+    await fs.promises.access(filePath, fs.constants.F_OK);
+  } catch (error) {
+    return { status: WEBHOOK.FAILURE, message: `Exported data could not be found at ${filePath}` };
+  }
+
+  // Produce checksum of the file using sha256
+  let checksum: string | null = null;
+
+  try {
+    const fileContent = await fs.promises.readFile(filePath);
+    const hash = createHash('sha256');
+    hash.update(fileContent);
+    checksum = hash.digest('base64');
+  } catch (error) {
+    return {
+      status: WEBHOOK.FAILURE,
+      message: `Exported data  at ${filePath} could not be accessed for further processing.`,
+    };
+  }
+
+  // Prepare payload for signature.
+  let payload = {
+    id: nconf.get('appID'),
+    type: payloadType,
+    fileName,
+    checksum,
+  };
+
+  // Sort payload alphabetically.
+  const sortedPayload = Object.fromEntries(Object.entries(payload).sort());
+
+  // Sign the sortedPayload after converting it to a string.
+  const payloadString = JSON.stringify(sortedPayload);
+  const hash = createHash('sha512').update(payloadString).digest();
+  const signature = sign(null, hash, 'privateKey').toString('base64');
+
+  // Add signature to the final payload.
+  payload = { ...payload, signature };
+
   return { status: WEBHOOK.SUCCESS };
 };
